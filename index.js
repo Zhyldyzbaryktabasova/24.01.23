c3da4f2c4e243934540ee08c2f7c0e71a853f1c1
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-01-25 16:25:44 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,107 @@
+//let myName = "Jyldyz";
+//let myCity = "Karakol";
+//let myCar = "Lexus";
+//let myTelephon = "Samsung";
+//let myFavoriteFood = "Manty";
+//let myColor = "While";
+//let myStreed= "Lenina";
+//let myFavoriteWatch = "Quarts";
+//let myFavoriteName ="Aygul";
+//let weekday = "Tuesday";
+
+
+
+
+//let pi1 = 123.002;
+//let pi2  = 12.445;
+//let pi3 = 77.556;
+//let pi4 = 4.545;
+//let pi5 = 558;
+
+
+
+
+//let place = 20;
+//let tourist = 23;
+//if (place<= tourist){
+  //console.log("Туристы не поместятся")
+//}
+//else {
+//console.log("Туристы  поместятся")
+//}
+
+//let example1 = 5 * 5;
+//let example2 = 4 - 2;
+//let example3 = 5 + 5;
+//let example4 = 10 / 2;
+//let example5 = 8 * 3;
+//let example6 = 7 + 1;
+//let example7 = 6 * 6;
+//let example8 = 9 / 3;
+//let example9 = 6 - 5;
+//let example10 = 1 + 1;
+
+//let ex1 = 6>3;
+//let ex2 = 4<10;
+//let ex3 = 2>5;
+//let ex4 = 5=5;
+//let ex5 = 7<8;
+
+//let a = 5;
+//b = 5;
+//c = 5;
+
+//let result;
+//result = 5;
+//result = 3+8;
+//result = false;
+//result = "Hello";
+//console.log(result);
+
+//let number = 3;
+//if (number > 0) {
+  //console.log("Число" + number + "положительное");
+//}
+//else if (number < 0) {
+//console.log("Число" + number + "отрицательное");
+//}
+//else if (number ===0) {
+  //console.log("Число ноль не отрицательное и не положительно");
+//}
+//else {
+  //console.log(number + "не является числом");
+//}
+
+//Является ли число 5 четным или нечетным числом.
+let num = 5;
+if (num %2 === 0) {
+  console.log(num +"is even");
+}
+else if (num %2 ===1) {
+  console.log(num +"is odd")
+}
+else {
+  console.log(num + "is not a number");
+}
+// У Васи 212 сомов, хватить ли ему денег на мороженное которая стоит 40 сом.
+let cash = 212;
+let iceCream = 40;
+if (cash >= iceCream){
+  console.log("Вася купит мороженное");
+}
+else {
+  console.log ("Вася не купит мороженное");
+}
+//Вывести  каким сезоном является ти "Январь"?
+let month = "январь";
+if (month === "декабрь" || month === "январь" || month === "февраль") {
+  console.log("Это зима")
+}
+//Является ли "Пятница" будним или выходным днем.
+let day = "пятница";
+if (day === "пятница" ) {
+  console.log("Это будним  днем")
+} 
+else {
+  console.log("Это не будним днем")
+}
